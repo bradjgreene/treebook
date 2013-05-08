d6ea62da8b2dc848193b0e74c2f80a8e09edb2ba
Brad Greene <brad@bradjgreene.com>
2013-05-07 23:31:26 -0500

    treebook with bootcamp installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ef8ca30a215a01b9ef3f363303a51481659f5528a9aea8f1f6c55355cc0923d7fb5c8715c77a4d8a5ce96b37707afd8c2d47100547fc9e833c4efb01af41ae32'
